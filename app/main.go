2fe800121aa01ee1226337e5ce85733b213cba37
NikitaYurchyk <ny.yurchyk@gmail.com>
2026-08-05 22:25:23 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"strings"
 )
 
 func handleClient(conn net.Conn, store *Store) {
@@ -41,8 +42,8 @@ func handleClient(conn net.Conn, store *Store) {
 
 func main() {
 	port := flag.Int("port", 6379, "port to listen on")
+	replicaOf := flag.String("replicaof", "", "master to replicate, as \"<host> <port>\"")
 	flag.Parse()
-
 	addr := fmt.Sprintf(":%d", *port)
 
 	listener, err := net.Listen("tcp", addr)
@@ -50,10 +51,19 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	defer listener.Close()
 
 	store := NewStore()
 	store.info = InitInfo()
+	if *replicaOf != "" {
+		fields := strings.Fields(*replicaOf)
+		if len(fields) != 2 {
+			panic("--replicaof needs \"<host> <port>\"")
+		}
+		store.info.Replication.Role = RoleSlave
+	}
+
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
